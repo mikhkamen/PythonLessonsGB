fe18f492bb877c572a7a87a76ac235e97a308c50
Mikhail <mikhkamen@gmail.com>
2021-11-13 15:56:13 +0200

    cells modified

--- lesson_7/cells.py
+++ lesson_7/cells.py
@@ -1,24 +1,23 @@
-class Cell:
+class Cells:
     def __init__(self, number):
         self.number = number
         self.cell = '*'
 
     def __sub__(self, other):
         if self.number >= other.number:
-            return Cell(self.number - other.number)
-        print(f'Операция недопустима: количество клеток\n'
-              f' в первом объекте ({self.number}) меньше,'
-              f' чем во втором ({other.number})')
-        return Cell(-1)
+            return Cells(self.number - other.number)
+        print(f'Количество клеток в первом объекте ({self.number}) меньше,'
+              f' чем во втором ({other.number}).\nБудет создан объект Cells(0)')
+        return Cells(0)
 
     def __mul__(self, other):
-        return Cell(self.number * other.number)
+        return Cells(self.number * other.number)
 
     def __truediv__(self, other):
-        return Cell(self.number // other.number)
+        return Cells(int(round(self.number / other.number, 0)))
 
     def __add__(self, other):
-        return Cell(self.number + other.number)
+        return Cells(self.number + other.number)
 
     def make_order(self, n):
         rest = self.number
@@ -28,20 +27,20 @@ class Cell:
         print(rest * self.cell, '\n')
 
 
-cell_1 = Cell(20)
-cell_2 = Cell(23)
+cells_1 = Cells(20)
+cells_2 = Cells(23)
 
-res_add = cell_1.__add__(cell_2)
-print(res_add.number)
+cells_res_add = cells_1.__add__(cells_2)
+print(cells_res_add.number)
 
-res_sub = cell_1.__sub__(cell_2)
-print(res_sub.number)
+cells_res_sub = cells_1.__sub__(cells_2)
+print(cells_res_sub.number)
 
-res_mul = cell_1.__mul__(cell_2)
-print(res_mul.number)
+cells_res_mul = cells_1.__mul__(cells_2)
+print(cells_res_mul.number)
 
-res_div = cell_1.__truediv__(cell_2)
-print(res_div.number)
+cells_res_div = cells_1.__truediv__(cells_2)
+print(cells_res_div.number)
 
-cell_1.make_order(7)
-cell_2.make_order(6)
+cells_1.make_order(7)
+cells_2.make_order(6)
