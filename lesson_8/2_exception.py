4f2b039dbafad8dc3a41da6f4ed547b2d40e5c6c
Mikhail <mikhkamen@gmail.com>
2021-11-18 18:09:43 +0200

    exception & class_Comlex added

---
+++ lesson_8/2_exception.py
@@ -0,0 +1,20 @@
+class MyError(Exception):
+    def __init__(self, text):
+        self.text = text
+
+
+# Пример:
+try:
+    n = int(input("Введите делимое: \n"))
+    m = int(input('Введите делитель: \n'))
+    l = n / m
+    if m == 1:
+        raise MyError("На единицу  делить нельзя!")
+except ValueError:
+    print("Вы ввели не число")
+except ZeroDivisionError:
+    print('На ноль делить нельзя!')
+except MyError as err:
+    print(err)
+else:
+    print(l)
